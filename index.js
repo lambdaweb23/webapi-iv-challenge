069af5a850a4e4db9e97bc784a2fff91febf562d
Kenna <kennaalastair@gmail.com>
2019-10-31 14:58:51 -0700

    changed message in GET

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log('\nmessage:', process.env.MSG);
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: process.env.MSG });
+    res.status(200).send("Hello World");
 });
 
 // the port is coming from the environment
